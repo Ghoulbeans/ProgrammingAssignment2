ca732be7c5caeb08eb9e0574c88956dcb399365f
Alex Durrant <alexfromtheinternet@pm.me>
2023-02-10 21:00:43 +1100

    Update cachematrix.R
    Version 1 (untested)

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,38 @@
-## Put comments here that give an overall description of what your
-## functions do
+## This code uses scoping rules to cache values while producing the 
+## inverse of an invertable matrix. This process is closely based off of the 
+## mean vector demo 
 
-## Write a short comment describing this function
+## As with the demo, this function sets & gets the matrix, then sets & gets the 
+## value of the inverse of the matrix
 
 makeCacheMatrix <- function(x = matrix()) {
-
+    nvrs <- NULL
+    set <- function(y){
+        x <<- y
+        nvrs <<- NULL
+    }
+    
+    get <- function() x
+    setinverse <- function(inverse) nvrs <- inverse
+    getinverse <- function() nvrs
+    list(set = set, get = get)
+        setinverse = setinverse
+        getinverse = getinverse
 }
 
 
-## Write a short comment describing this function
+## This function checks to see if the inverse has already been set, and 
+## retrieves it if so (with a message). If not, it runs a solve function to 
+## create the inverse and sets it.
 
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+        nvrs <- x$getinverse()
+        if(!is.null(nvrs)) {
+            message("one sec, I'm fetching your cached inverse data!")
+            return(nvrs)
+        }
+        data <- x$get()
+        nvrs <- solve(matr,...)
+        x$setinverse()
+        nvrs
 }
